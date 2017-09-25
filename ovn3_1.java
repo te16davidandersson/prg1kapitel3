b2fe96ba962400e1f0233fedd50c0895707cc215
David Andersson <anda16@nti.umea>
2017-09-25 10:30:00 +0200

    ovn3_1

---
+++ ovn3_1.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+/* Övning 3.1
+David Andersson
+25/9 2017 */
+
+public class ovn3_1{
+	public static void main(String[] args){
+
+		int i;
+		double d;
+		byte b;
+		i = 500;
+		d = 500;
+		b = 500;  //Byte var inte tillåten för den accepterar bara -127->127
+	}
+}
