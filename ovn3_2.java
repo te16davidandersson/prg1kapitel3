300236b4639499e25ccca75e06013b8853c03b37
David Andersson <anda16@nti.umea>
2017-09-25 10:35:24 +0200

    ovn3_2

---
+++ ovn3_2.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+/* Övning 3.2
+David Andersson
+25/9 2017 */
+
+public class ovn3_2{
+	public static void main(String[] args){
+
+		int i;
+		double d;
+		byte b;
+		i = 500.7;
+		d = 500.7;
+		b = 500.7;
+
+		// Bara d fungerar eftersom bara double kan hantera reella tal.
+	}
+}
